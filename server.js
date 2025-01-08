a021de5c43992052ed057fb55fe3c05090c353d1
Muhammad Anas <anassoormo775@gmail.com>
2025-01-08 16:19:22 +0500

    kuch UPDATE ON API IS DONE

--- server.js
+++ server.js
@@ -1,42 +1,43 @@
-import express, { request, response } from 'express'
-const app = express()
-const port = 5001
+import express, { request, response } from "express";
+const app = express();
+const port = process.env.PORT || 5001;
 const todos = [];
 
+app.use(express.json()); // to convert body into json (body = ppost man body jis ma sare encrypted data hota ha)
 
-
-app.use(express.json())
-
-// yah api sa todo ko lena ka lia ha 
-app.get('/all-todos', (request, response) => {
-  response.send(todos)
-})
-
+// yah api sa todo ko lena ka lia ha
+app.get("/all-todos", (request, response) => {
+  response.send(todos);
+});
 
 // for post on new todo on browser
 app.post("/add-todo", (request, response) => {
-  todos.push(request.body.todo)
-  response.send("send first todo")
-})
+  todos.push({ todoContent: request.body.todo, id: new Date().getTime() });
+  response.send("first todo");
+});
 
-// for edit only single todo 
-app.patch('/edit-todo/:id', (request, response) => {
+// for edit only single todo
+app.patch("/edit-todo/:id", (request, response) => {
+  const id = parseInt(request.params.id);
+  for (let index = 0; index < todos.length; index++) {
+    if (todos[index].id === id) {
+      // Compare with converted number
+      todos[index].todoContent = request.body.todoContent;
+      break;
 
-})
+    }
+  }
+  response.status(201).send("Todo Updated Sucussfully!");
+});
 
-//for delete only single todo 
-app.delete('/delete-solo-todo/:id', (request, response) => {
-
-})
-
-
-
+//for delete only single todo
+app.delete("/delete-solo-todo/:id", (request, response) => {});
 
 //
-app.use((request, response) =>{
-  response.status(404).send('No Route Found 404')
-})
+app.use((request, response) => {
+  response.status(404).send("No Route Found 404");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
